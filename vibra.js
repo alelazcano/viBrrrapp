f797ca69eeb294a7e1ca2cb950065e60ed457105
alelazcano <47332889+alelazcano@users.noreply.github.com>
2022-06-24 22:34:52 -0300

    foo

--- vibra.js
+++ vibra.js
@@ -3,8 +3,13 @@ function detectarDispositivo() {
     const toMatch = [/Android/i, /webOS/i, /iPhone/i, /iPad/i, /iPod/i, /BlackBerry/i, /Windows Phone/i];
     let queDispositivo = toMatch.some((toMatchItem) => { return navigator.userAgent.match(toMatchItem);});
 
+<<<<<<< HEAD
     if (queDispositivo == true) {
         document.getElementById("aviso-compatibilidad").style="visibility: hidden;";
+=======
+    if (queDispositivo === true) {
+        document.getElementById("aviso-compatibilidad").style="visibility: visible;";
+>>>>>>> 12bdfd3495aff3336b4241c21d1876ffafc6e980
         console.log("detectó que NO es mobile y muestra el aviso de compatibilidad");
       } // ELSE: no se necesitó la variable "si es PC", pero se puede perfeccionar el IF con otros casos
 
@@ -25,10 +30,6 @@ function vib(param){
         window.navigator.vibrate([200, 100, 200, 100, 200, 100, 200, 100, 200, 100, 200, 100, 200, 100, 200, 100, 200, 100, 200, 100]);
         break;
 
-        case '2':
-        console.log(2);
-        break;
-
         case '3':
         console.log(3);
         break;
@@ -44,6 +45,9 @@ function vib(param){
         case '6':
         console.log(6);
         break;
+
+        default:
+            console.log("default case");
     }
 }
 
